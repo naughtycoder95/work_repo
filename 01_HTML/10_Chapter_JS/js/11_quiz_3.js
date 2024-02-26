855d8ef7af6e5bde608ffca9dcbd9d63ecb08db6
KimTaewan <158536479+naughtycoder95@users.noreply.github.com>
2024-02-26 16:04:01 +0900

    Delete 01_HTML directory

--- 01_HTML/10_Chapter_JS/js/11_quiz_3.js
+++
@@ -1,7 +0,0 @@
-// 11_quiz_3.js
-// 버튼 클릭 함수 정의
-function changeColor() {
-    // TODO: 1) p 태그를 접근(선택)해서 2) 빨간색으로 색깔 적용
-    document.querySelector("p").style.color = "red";
-
-}
--- 01_HTML/10_Chapter_JS/js/22_dom_handing_classList.js
+++
@@ -1,8 +0,0 @@
-// 22_dom_handing_classList.js
-// TODO: classList (특수)속성 소개
-// TODO: 1) #content div 태그 선택
-let content = document.querySelector("#content"); // div 태그
-
-// TODO: 빨간색 클래스 추가
-// TODO: css 의 .red 클래스를 추가
-content.classList.add("red");
